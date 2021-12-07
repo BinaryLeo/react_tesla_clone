07910e7cbae5658764b4814a0ad3b9110d3b5726
binaryleo <leomouracss@fastmail.com>
2021-12-06 21:39:10 -0300

    ADD:  Hamburger Menu and React Reveal

--- tesla-clone/src/Components/Header.js
+++ tesla-clone/src/Components/Header.js
@@ -1,48 +1,73 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
 function Header() {
-    return (
-        <Container>
-            <a href>
-            <img src="/images/logo.svg" alt=""/>
-            </a>
-            <Menu>
-            <p><a href="#">Model S</a></p>
-            <p><a href="#">Model 3</a></p>
-            <p><a href="#">Model X</a></p>
-            <p><a href="#">Model Y</a></p>
-            <p><a href="#">Solar Roof</a></p>
-            <p><a href="#">Solar Panels</a></p>
-            </Menu>
-        </Container>
-    )
+  return (
+    <Container>
+      <a href ="/#">
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        <a href="/#">Model S</a>
+        <a href="/#">Model 3</a>
+        <a href="/#">Model X</a>
+        <a href="/#">Model Y</a>
+        <a href="/#">Solar Roof</a>
+        <a href="/#">Solar Panels</a>
+      </Menu>
+      <RightMenu>
+        <a href="/#">Shop</a>
+        <a href="/#">Tesla Account</a>
+        <CustomMenu/>
+      </RightMenu>
+    
+    </Container>
+  );
 }
 
-export default Header
+export default Header;
 
 const Container = styled.div`
-min-height:60px;
-position: fixed;
-display:flex;
-align-itens: center;
-padding: 0 20px;
-top:0;
-left:0;right:0;
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-itens: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
 
-img{
-    padding:20px;
-}
-`
+  img {
+    padding: 20px;
+  }
+`;
 const Menu = styled.div`
-display:flex;
-align-items: center;
-justify-content:center;
-flex:1;
-p{
-    font-size:14px;
-    font-weight:600;
-    text-transform:uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  a {
+    font-size: 14px;
+    font-weight: 600;
+    text-transform: uppercase;
     padding: 0 10px;
     flex-wrap: nowrap;
-}
-`
+  }
+  @media(max-width: 768px) {
+      display:none;
+  }
+`;
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+ 
+  a {
+    font-size: 14px;
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    flex-wrap: nowrap;
+  }
+`;
+const CustomMenu = styled(MenuIcon)`
+cursor: pointer`;
